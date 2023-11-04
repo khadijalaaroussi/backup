b431900d09b5e1cd24bf2d4213dae793850e575a
Khadija Laaroussi <I00640@intelcia.com>
2023-11-04 08:29:19 +0100

    backup tempo

---
+++ src/main/java/com/example/Back_Fin/BackFinApplication.java
@@ -0,0 +1,13 @@
+package com.example.Back_Fin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Back_Fin/BackFinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Back_Fin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
